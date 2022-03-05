a8e3d59c5d325af8c4e0e8cf628eb647fef2c2b0
Gavin Wentzel <gavinwentzel@gmail.com>
2022-03-04 18:37:48 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -88,8 +89,64 @@ function init() {
         name: 'contact',
       },
   ])
+
+  // const { type, message, name } = inquirer;
   .then((response) => {
+    const template = `# <Your-Project-Title>
+
+    ## Description (Motivation? The why? Solved Problems?)
+    
+      ${response.description}
+    
+    ## Table of Contents 
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    
+    ## Installation
+
+    ${response.installation}
+    
+    ## Usage
+    
+    To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+    
+       md
+        ![alt text](assets/images/screenshot.png)
+       
+    
+    ## Credits
+    
+    
+    
+    ## License
+    
+    
+    ## Badges
+    
+    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+    
+    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+    
+    ## Features
+    
+    If your project has a lot of features, list them here.
+    
+    ## How to Contribute
+    
+    If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+    
+    ## Tests
+    
+    Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+    
     console.log(response);
+    fs.writeFile("template.md", template, (err) => {
+        err ? console.error(err) : console.log("File has been written");
+    }
+    )
 
   }
     
---
+++ template.md
@@ -0,0 +1,48 @@
+# <Your-Project-Title>
+
+## Description (Motivation? The why? Solved Problems?)
+
+
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+
+## Usage
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+
+
+## License
+
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
